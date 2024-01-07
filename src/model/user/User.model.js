80496713e3fae0653cbd6eba5475271fe09eaf33
Kenyiro Anazagasty <kenyicode@gmail.com>
2024-01-07 13:53:14 -0500

    added user authentication

--- src/model/user/User.model.js
+++ src/model/user/User.model.js
@@ -1,5 +1,6 @@
 const {UserSchema} = require("./User.schema")
 
+//Inserts new user in db
 const insertUser = (userObj) => {
     return new Promise((resolve, reject) => {
         UserSchema(userObj).save()
@@ -8,6 +9,18 @@ const insertUser = (userObj) => {
     })
 }
 
+//Checks for existing user by email
+const getUserByEmail = (email) => {
+    return new Promise((resolve, reject) => {
+        if (!email) return false
+
+        UserSchema.findOne({email})
+        .then((data) => resolve(data))
+        .catch((error) => reject(error))
+        })
+}
+
 module.exports = {
-    insertUser
+    insertUser,
+    getUserByEmail
 }
--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
-const {insertUser} = require("../model/user/User.model")
-const {hashPassword} = require("../helpers/bcrypt.helper")
+const {insertUser, getUserByEmail} = require("../model/user/User.model")
+const {hashPassword, comparePassword} = require("../helpers/bcrypt.helper")
 
 router.all("/", (req, res, next) => {
     //res.json({ message: "Return form user router" })
@@ -9,6 +9,7 @@ router.all("/", (req, res, next) => {
     next()
 })
 
+//New user router
 router.post("/", async (req, res) => {
     const {name, phone, email, password} = req.body
     try {
@@ -31,6 +32,31 @@ router.post("/", async (req, res) => {
 
         res.json({status: "error", message: error.message})
     }
+})
+
+//Sign in router
+router.post("/login", async (req, res) => {
+    const {email, password} = req.body
+
+    if (!email || !password) {
+        return res.json({status: "error", message: "Form submission error"})
+    }
+
+    // Confirm account exists and obtaining hashed pw
+    const user = await getUserByEmail(email)
+    const passFromDb = user && user._id ? user.password : null
+
+    if (!passFromDb) {
+        return (
+            res.json({status: "error", message: "Invalid email or password"})
+        )
+    }
+
+    // Compare plaintext password with obtained hashed pw
+    const result = await comparePassword(password, passFromDb)
+    console.log(result)
+    res.json({status: "Success", message: "Login successful"})
+
 })
 
 module.exports = router
